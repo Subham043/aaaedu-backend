e7a4b4860c4f7eb5a0c893bd3ea3a4fd963fb82e
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-12-16 09:04:20 +0530

    completed graph for reports

--- src/resources/views/admin/pages/test/taken/report.blade.php
+++ src/resources/views/admin/pages/test/taken/report.blade.php
@@ -15,14 +15,11 @@
             @include('admin.includes.back_button', ['link'=>route('test.taken.paginate.get')])
             <div class="col-lg-12">
                 <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title mb-0">Test Report</h4>
-                    </div><!-- end card header -->
 
-                    <div class="text-center bg-dark py-2">
+                    <div class="text-center bg-danger py-2">
                         <h4 class="text-light m-0">TEST INFO</h4>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body pb-0">
                         <div id="customerList">
                             <div class="table-responsive table-card">
                                 <table class="table align-middle table-nowrap" id="customerTable">
@@ -55,14 +52,14 @@
                         </div>
                     </div><!-- end card -->
 
-                    <div class="text-center bg-dark py-2">
+                    <div class="text-center bg-warning py-2">
                         <h4 class="text-light m-0">SUBJECT WISE EVALUATION</h4>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body pb-0">
                         <div id="customerList">
                             <div class="table-responsive table-card">
                                 <table class="table align-middle table-nowrap" id="customerTable">
-                                    <thead class="table-light">
+                                    <thead class="table-light text-center">
                                         <tr>
                                             <th class="sort" data-sort="customer_name">Subject</th>
                                             <th class="sort" data-sort="customer_name">Total Questions</th>
@@ -71,14 +68,14 @@
                                             <th class="sort" data-sort="customer_name">Marks Alloted</th>
                                             </tr>
                                     </thead>
-                                    <tbody class="list form-check-all">
-                                        @foreach($subject_wise_score as $subject_wise_score)
+                                    <tbody class="list form-check-all text-center">
+                                        @foreach($subject_wise_score as $val)
                                         <tr>
-                                            <td class="customer_name">{{ $subject_wise_score->name }}</td>
-                                            <td class="customer_name">{{ $subject_wise_score->number_of_question }}</td>
-                                            <td class="customer_name">{{ $subject_wise_score->attempt_count }}</td>
-                                            <td class="customer_name">{{ $subject_wise_score->total_mark_sum }}</td>
-                                            <td class="customer_name">{{ $subject_wise_score->total_mark_alloted_sum }}</td>
+                                            <td class="customer_name">{{ $val->name }}</td>
+                                            <td class="customer_name">{{ $val->number_of_question }}</td>
+                                            <td class="customer_name">{{ $val->attempt_count }}</td>
+                                            <td class="customer_name">{{ $val->total_mark_sum }}</td>
+                                            <td class="customer_name">{{ $val->total_mark_alloted_sum }}</td>
                                         </tr>
                                         @endforeach
                                     </tbody>
@@ -86,15 +83,20 @@
                             </div>
                         </div>
                     </div><!-- end card -->
+                    <div class="w-100 p-2 d-flex justify-content-center">
+                        <div class="col-lg-6 col-md-6 col-sm-12">
+                            <canvas id="subject-wise-id"></canvas>
+                        </div>
+                    </div>
 
-                    <div class="text-center bg-dark py-2">
+                    <div class="text-center bg-primary py-2">
                         <h4 class="text-light m-0">TOTAL EVALUATION</h4>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body pb-0">
                         <div id="customerList">
-                            <div class="table-responsive table-card mb-1">
+                            <div class="table-responsive table-card">
                                 <table class="table align-middle table-nowrap" id="customerTable">
-                                    <thead class="table-light">
+                                    <thead class="table-light text-center">
                                         <tr>
                                             <th class="sort" data-sort="customer_name">Total Questions</th>
                                             <th class="sort" data-sort="customer_name">Questions Attempted</th>
@@ -102,7 +104,7 @@
                                             <th class="sort" data-sort="customer_name">Marks Alloted</th>
                                             </tr>
                                     </thead>
-                                    <tbody class="list form-check-all">
+                                    <tbody class="list form-check-all text-center">
                                         <tr>
                                             <td class="customer_name">{{ $total_question_count }}</td>
                                             <td class="customer_name">{{ $total_answer_count }}</td>
@@ -115,6 +117,12 @@
                         </div>
                     </div><!-- end card -->
 
+                    <div class="w-100 p-2 d-flex justify-content-center">
+                        <div class="col-lg-4 col-md-6 col-sm-12">
+                            <canvas id="total-wise-id"></canvas>
+                        </div>
+                    </div>
+
                 </div>
                 <!-- end col -->
             </div>
@@ -125,4 +133,57 @@
     </div>
 </div>
 
+@stop
+
+@section('javascript')
+<script src="{{ asset('admin/js/pages/plugins/chart.umd.min.js') }}"></script>
+<script type="text/javascript" nonce="{{ csp_nonce() }}">
+    const subject_wise_score_data = @json($subject_wise_score);
+    const labels = subject_wise_score_data.map(item => item.name);
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Subject Wise Evaluation (%)',
+            data: subject_wise_score_data.map(item => ((item.total_mark_alloted_sum / item.total_mark_sum)*100).toFixed(2)),
+        }]
+    };
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            // indexAxis: 'y',
+            scales: {
+                x: {
+                    stacked: true
+                },
+                y: {
+                    beginAtZero: true,
+                    stacked: true
+                }
+            }
+        },
+    };
+    new Chart(document.getElementById('subject-wise-id'), config);
+
+    const total_data = {{$total_score}};
+    const alloted_data = {{$alloted_score}};
+    const data2 = {
+        labels: [
+            'Total %',
+            'Alloted %'
+        ],
+        datasets: [{
+            label: 'Total Evaluation (%)',
+            data: [
+                ((total_data/total_data)*100).toFixed(2),
+                ((alloted_data/total_data)*100).toFixed(2)
+            ],
+        }]
+    };
+    const config2 = {
+        type: 'pie',
+        data: data2,
+    };
+    new Chart(document.getElementById('total-wise-id'), config2);
+</script>
 @stop
