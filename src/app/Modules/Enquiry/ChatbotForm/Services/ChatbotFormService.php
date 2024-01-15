a1f49aa558b1605ce5e118517a5a5cc81f110360
Subham Saha <subham@Subhams-MacBook-Pro.local>
2024-01-15 15:03:04 +0530

    done

--- src/app/Modules/Enquiry/ChatbotForm/Services/ChatbotFormService.php
+++ src/app/Modules/Enquiry/ChatbotForm/Services/ChatbotFormService.php
@@ -34,7 +34,7 @@ class ChatbotFormService
         return ChatbotForm::findOrFail($id);
     }
 
-    public function getByLeadId(int $lead_id): ChatbotForm|null
+    public function getByLeadId($lead_id): ChatbotForm|null
     {
         return ChatbotForm::where('lead_id', $lead_id)->firstOrFail();
     }
